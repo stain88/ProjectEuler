87a5a24180887186f56a435090ec2df75572b8e5
stain88 <marcbaghdadi@gmail.com>
2018-03-21 17:21:52 +0000

    JS 0001

--- 0001/javascript/main.js
+++ 0001/javascript/main.js
@@ -1,3 +1,4 @@
 var a = Array.from(Array(1000).keys());
-a = a.filter(function(i) {return i%3===0 || i%5===0});
-a.reduce(function(i,j) {return i+j});
+a = a.filter(function(i) {return i%3===0 || i%5===0;});
+a = a.reduce(function(i,j) {return i+j;});
+console.log(a);
